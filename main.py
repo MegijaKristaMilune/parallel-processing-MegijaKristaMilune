869043194c5770b57f74ce59e7272a075633d253
MegijaKristaMilune <125242855+MegijaKristaMilune@users.noreply.github.com>
2023-03-18 11:48:18 +0000

    test

--- main.py
+++ main.py
@@ -8,24 +8,7 @@ def parallel_processing(n, m, data):
     return output
 
 def main():
-    # TODO: create input from keyboard
-    # input consists of two lines
-    # first line - n and m
-    # n - thread count 
-    # m - job count
-    n = 0
-    m = 0
-
-    # second line - data 
-    # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-    data = []
-
-    # TODO: create the function
-    result = parallel_processing(n,m,data)
     
-    # TODO: print out the results, each pair in it's own line
-
-
 
 if __name__ == "__main__":
     main()
